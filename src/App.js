7efd1a642dc506d229450fcbccb05b667761ebbc
Felipe Ferreira <felipesqf@gmail.com>
2021-01-23 13:59:51 +1100

    change remove routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 import Employees from "./pages/Employees";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
-    <Router>
       <div>
         <Wrapper>
-          <Route exact path="/" component={Employees} />
+          <Employees />
         </Wrapper>
         <Footer />
       </div>
-    </Router>
   );
 }
 
